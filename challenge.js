04f444c1f9d79d2d04cb3783fc5cbe3a3c590c62
Russell Lee <sjkrlee50@gmail.com>
2023-04-19 17:02:51 -0400

    more fundamentals

--- challenge.js
+++ challenge.js
@@ -1,5 +1,5 @@
 "use strict";
-
+/*
 const calcAverage = (value1, value2, value3) => (value1 + value2 + value3) / 3;
 
 const dolphinsAverage1 = calcAverage(44, 23, 71);
@@ -22,3 +22,19 @@ function checkWinner(avgDolphins, avgKoalas) {
 
 checkWinner(dolphinsAverage1, koalasAverage1);
 checkWinner(dolphinsAverage2, koalasAverage2);
+*/
+
+const bills = [125, 555, 44];
+
+const calcTip = function (bill) {
+  if (bill >= 50 && bill <= 300) {
+    return bill * 0.15;
+  } else {
+    return bill * 0.2;
+  }
+};
+
+const tips = [calcTip(bills[0]), calcTip(bills[1]), calcTip(bills[2])];
+const totals = [bills[0] + tips[0], bills[1] + tips[1], bills[2] + tips[2]];
+
+console.log(bills, tips, totals);
